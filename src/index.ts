29b4778535fe7b29971b65aef1e653853d7d778f
SRINJOY DAS <srinjoydas1104@gmail.com>
2025-02-27 05:57:59 +0530

    prod fix lil

--- src/index.ts
+++ src/index.ts
@@ -119,7 +119,7 @@ app.post('/chat' , async (req,res) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT , () => {
     console.log(`server is running on port : ${PORT}`);
 })
